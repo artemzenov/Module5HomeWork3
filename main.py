a226925b1346ea08dd189ab57c1cf24647b9136b
Artem Zenov <zenovae1987@gmail.com>
2024-10-02 20:42:40 +0500

    Create Module5HomeWork3

---
+++ main.py
@@ -0,0 +1,26 @@
+from developer.class_dev import House
+
+
+if __name__ == '__main__':
+    h1 = House('ЖК Эльбрус', 10)
+    h2 = House('ЖК Акация', 20)
+
+    print(h1)
+    print(h2)
+    print(h1 == h2)  # __eq__
+
+    h1 = h1 + 10  # __add__
+    print(h1)
+    print(h1 == h2)
+
+    h1 += 10  # __iadd__
+    print(h1)
+
+    h2 = 10 + h2  # __radd__
+    print(h2)
+
+    print(h1 > h2)  # __gt__
+    print(h1 >= h2)  # __ge__
+    print(h1 < h2)  # __lt__
+    print(h1 <= h2)  # __le__
+    print(h1 != h2)  # __ne__
